9a19f0f670c15b9b9a2a07bc85861da3d29ecb14
GiovannyLESM <72723304@continental.edu.pe>
2021-10-01 16:32:09 -0500

    T commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const { ResultWithContext } = require('express-validator/src/chain');
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('./pages/index');
+    res.render('./pages/index', {
+        title: 'TiendaGo',
+        Style: 'styles.css',
+        links: 'https://fonts.googleapis.com',
+        links2: 'https://fonts.gstatic.com',
+        links3: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap',
+        crossorigin: 'crossorigin',
+    });
 })
 module.exports = router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 router.get('/catalogue', (req, res) => {
-    res.render('./pages/catalogue');
+    res.render('./pages/catalogue', {
+        title: 'Catálogos',
+        Style: 'normalize_cata.css',
+        Style2: 'skeleton_cata.css',
+        Style3: 'style_cata.css',
+    });
 });
 module.exports = router;
